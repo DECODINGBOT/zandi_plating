f3e80c75257013bec02acb13d03a3e3c544e5056
UNKNOWN <kmhyns@hanyang.ac.kr>
2025-01-02 23:12:58 +0900

    push test

---
+++ .vscode/2741.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main(void) {
+	unsigned int n;
+	cin >> n;
+	for (int i = 1; i < n + 1; i++) {
+		cout << i << '\n';
+	}
+	return 0;
+}
---
+++ 2577.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+/*
+int main(void) {
+	int arr[10] = { 0, };
+	unsigned int a, b, c, x;
+	cin >> a;
+	cin >> b;
+	cin >> c;
+	x = a * b * c;
+
+	while (x >= 1) {
+		int remainder = x % 10;
+		arr[remainder] += 1;
+		x /= 10;
+	}
+
+	for (int i = 0; i < 10; i++) {
+		cout << arr[i] << endl;
+	}
+	return 0;
+}*/
